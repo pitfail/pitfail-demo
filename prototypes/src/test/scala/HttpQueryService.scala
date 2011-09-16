e9b5fe3f3454ab7383bae213163886f5f12088ad
Michael Koval <koval.michael@gmail.com>
2011-09-15 22:00:42 -0400

    Switched from FunSuite to Spec for unit tests.

--- prototypes/src/test/scala/HttpQueryService.scala
+++ prototypes/src/test/scala/HttpQueryService.scala
@@ -1,47 +1,53 @@
 package com.github.pitfail
 
-import org.scalatest.FunSuite
+import org.scalatest.Spec
 import org.scalatest.matchers.ShouldMatchers
 import scala.collection.immutable.ListMap
 
-class HttpQueryServiceTests extends FunSuite with ShouldMatchers {
-  test("buildQuery: Creates key-value pair.") {
-    val queryString = HttpQueryService.buildQuery(Map("a" -> "b"), "ASCII")
-    queryString should equal ("a=b")
+class HttpQueryServiceTests extends Spec with ShouldMatchers {
+  describe("buildQuery") {
+    it("creates key-value pair") {
+      val queryString = HttpQueryService.buildQuery(Map("a" -> "b"), "ASCII")
+      queryString should equal ("a=b")
+    }
+
+    it("delimits key-value pairs") {
+      val queryString = HttpQueryService.buildQuery(ListMap("a" -> "b", "c" -> "d"), "ASCII")
+      queryString should equal ("a=b&c=d")
+    }
+
+    it("encodes keys") {
+      val queryString = HttpQueryService.buildQuery(Map("a b" -> "c"), "ASCII")
+      queryString should equal ("a+b=c")
+    }
+
+    it("encodes values") {
+      val queryString = HttpQueryService.buildQuery(Map("a" -> "b c"), "ASCII")
+      queryString should equal ("a=b+c")
+    }
   }
 
-  test("buildQuery: Delimits key-value pairs.") {
-    val queryString = HttpQueryService.buildQuery(ListMap("a" -> "b", "c" -> "d"), "ASCII")
-    queryString should equal ("a=b&c=d")
-  }
+  describe("parseQuery") {
+    it("parses key-value pair") {
+      val params = HttpQueryService.parseQuery("a=b", "ASCII")
+      params should equal (Map("a" -> "b"))
+    }
 
-  test("buildQuery: Encodes keys.") {
-    val queryString = HttpQueryService.buildQuery(Map("a b" -> "c"), "ASCII")
-    queryString should equal ("a+b=c")
-  }
+    it("parses delimited key-value pairs") {
+      val params = HttpQueryService.parseQuery("a=b&c=d", "ASCII")
+      params should equal (ListMap("a" -> "b", "c" -> "d"))
+    }
 
-  test("buildQuery: Encodes values.") {
-    val queryString = HttpQueryService.buildQuery(Map("a" -> "b c"), "ASCII")
-    queryString should equal ("a=b+c")
-  }
+    it("decodes keys") {
+      val params = HttpQueryService.parseQuery("a+b=c", "ASCII")
+      params should equal (Map("a b" -> "c"))
+    }
 
-  test("parseQuery: Parses key-value pair.") {
-    val params = HttpQueryService.parseQuery("a=b", "ASCII")
-    params should equal (Map("a" -> "b"))
-  }
-
-  test("parseQuery: Parses delimited key-value pairs.") {
-    val params = HttpQueryService.parseQuery("a=b&c=d", "ASCII")
-    params should equal (ListMap("a" -> "b", "c" -> "d"))
-  }
-
-  test("parseQuery: Decodes keys.") {
-    val params = HttpQueryService.parseQuery("a+b=c", "ASCII")
-    params should equal (Map("a b" -> "c"))
-  }
-
-  test("parseQuery: Decodes values.") {
-    val params = HttpQueryService.parseQuery("a=b+c", "ASCII")
-    params should equal (Map("a" -> "b c"))
+    it("decodes values") {
+      val params = HttpQueryService.parseQuery("a=b+c", "ASCII")
+      params should equal (Map("a" -> "b c"))
+    }
   }
 }
+
+// vim: set ts=2 sw=2 et:
