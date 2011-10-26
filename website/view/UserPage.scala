1902a793ddfea84b3de48435b725fb27d11a399c
Owen Healy <ellbur@gmail.com>
2011-10-26 17:33:24 -0400

    User pages

--- website/view/UserPage.scala
+++ website/view/UserPage.scala
@@ -5,6 +5,8 @@ import net.liftweb.{common, http}
 import common._
 import http._
 
+import scala.xml._
+
 import intform._
 import errors._
 import Box._
@@ -13,14 +15,28 @@ import model.Schema._
 
 class UserPage extends Page with Loggable {
     
+    val nameParam = S param "name"
+    logger.info("Name is: " + nameParam)
+    
     def render = trans {
         import model.Schema._
         
         for {
-            name <- (S attr "name")  ?~ "No user specified"
+            name <- nameParam ?~ "No user specified"
             user <- byUsername(name) ?~ ("No user named " + name)
         }
-        yield TChart(user, false).render
+        yield {
+            lazy val all =
+                <div id="user-page">
+                    {header}
+                    {chart}
+                </div>
+            
+            lazy val chart = TChart(user, false).render
+            lazy val header = <h2>User: {name}</h2>
+            
+            all
+        }
     }
     .withMessage(m => <p>Sorry,{m}</p>)
     
