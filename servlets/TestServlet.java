4688213428663bfea4be5219f503ac18566993f3
Owen Healy <ellbur@gmail.com>
2011-10-26 16:42:53 -0400

    POST should work now

--- servlets/TestServlet.java
+++ servlets/TestServlet.java
@@ -28,24 +28,6 @@ public class TestServlet extends HttpServlet {
     )
         throws ServletException, IOException
     {
-        PrintWriter out = response.getWriter();
-        
-        // Get the 10 most recent events
-        List<NewsEvent> events = Schema.getRecentEvents(10);
-        for (NewsEvent event : events) {
-            out.printf("%s\n", event);
-        }
-        
-        // Buy a stock (in a GET no less!)
-        User user = Schema.ensureUser("ellbur_k_a");
-        Portfolio port = user.getMainPortfolio();
-        port.buyStock("ABC", new Dollars("23.00"));
-        
-        out.println();
-        out.printf("Cash: %s\n", port.cash().dollars());
-        for (StockAsset asset : port.getMyStockAssets()) {
-            out.printf("Asset: %s\n", asset);
-        }
     }
     
     @Override
@@ -55,6 +37,32 @@ public class TestServlet extends HttpServlet {
     )
         throws ServletException, IOException
     {
+        PrintWriter out = response.getWriter();
+        
+        // Get the 10 most recent events
+        List<NewsEvent> events = Schema.getRecentEvents(10);
+        for (NewsEvent event : events) {
+            out.printf("%s\n", event);
+        }
+        
+        // Buy a stock
+        User user = Schema.ensureUser("ellbur_k_a");
+        Portfolio port = user.getMainPortfolio();
+        port.buyStock("ABC", new Dollars("23.00"));
+        
+        try {
+            port.sellStock("ABC", new Dollars("23.00"));
+        }
+        catch (NotEnoughShares e) {
+            out.println("Failed cause you don't have enough cash");
+        }
+        
+        out.println();
+        out.printf("Cash: %s\n", port.cash().dollars());
+        for (StockAsset asset : port.getMyStockAssets()) {
+            out.printf("Asset: %s\n", asset);
+            out.printf("Shares = %s\n", asset.shares().shares());
+        }
     }
 }
 
