b6c3e60f3efc3fc05307e0d476b88d303ac24ab3
Cody Schafer <cpschafer@gmail.com>
2011-10-27 02:10:37 -0500

    tt/parser: indent.

--- texttrading/parser.scala
+++ texttrading/parser.scala
@@ -9,56 +9,56 @@ import model.{Shares,Dollars}
 
 object parser extends JavaTokenParsers {
 
-  def parseAction(text: String) = action(text)
+    def parseAction(text: String) = action(text)
 
-  lazy val action = opt(tags) ~ ( buy | sell | sellAll | showPortfolio ) ~ opt(tail) ^^ {
-    case _ ~ action ~ _ => action
-  }
-
-  lazy val tags = """([@#]\S*)+"""r
-
-  lazy val tail = """(\S+\s+)+"""r
-
-  lazy val buy = "buy" ~ stockAsset ^^ {
-    case _ ~ ass => Buy(ass)
-  }
-
-  lazy val sell = "sell" ~ stockAsset ^^ {
-    case _ ~ ass => Sell(ass)
-  }
-
-  lazy val sellAll = "sell" ~ ticker ^^ {
-    case _ ~ ticker => SellAll(ticker)
-  }
-
-  lazy val stockAsset = stockShares | stockVolume
-  lazy val stockShares = amount ~ ("shares" ~ opt("of")) ~ ticker ^^ {
-      case shares ~ _ ~ ticker => StockShares(ticker, Shares(shares))
+    lazy val action = opt(tags) ~ ( buy | sell | sellAll | showPortfolio ) ~ opt(tail) ^^ {
+        case _ ~ action ~ _ => action
     }
 
-  lazy val stockVolume = dollarAmount ~ (opt("dollars") ~ "of") ~ ticker ^^ {
-      case dollars ~ _ ~ ticker => StockDollars(ticker, Dollars(dollars))
-  }
+    lazy val tags = """([@#]\S*)+"""r
 
-  lazy val showPortfolio = (
-      opt( ( "show" ~ "me" ~ "my" ) )
-      ~ ("""portfolio\S*"""r) ) ^^ {
-    case _ ~ _ => Portfolio
-  }
+    lazy val tail = """(\S+\s+)+"""r
 
-  lazy val amount = decimalNumber ^^ {
-    case numText => BigDecimal(numText)
-  }
-
-  lazy val dollarAmount = (
-    amount
-    | "$" ~ amount ^^ {
-      case _ ~ amount => amount
+    lazy val buy = "buy" ~ stockAsset ^^ {
+        case _ ~ ass => Buy(ass)
     }
-  )
 
-  lazy val ticker = ident
+    lazy val sell = "sell" ~ stockAsset ^^ {
+        case _ ~ ass => Sell(ass)
+    }
 
-  implicit def toCSR(s: String): CharSequenceReader = new CharSequenceReader(s)
+    lazy val sellAll = "sell" ~ ticker ^^ {
+        case _ ~ ticker => SellAll(ticker)
+    }
+
+    lazy val stockAsset = stockShares | stockVolume
+    lazy val stockShares = amount ~ ("shares" ~ opt("of")) ~ ticker ^^ {
+        case shares ~ _ ~ ticker => StockShares(ticker, Shares(shares))
+    }
+
+    lazy val stockVolume = dollarAmount ~ (opt("dollars") ~ "of") ~ ticker ^^ {
+        case dollars ~ _ ~ ticker => StockDollars(ticker, Dollars(dollars))
+    }
+
+    lazy val showPortfolio = (
+        opt( ( "show" ~ "me" ~ "my" ) )
+        ~ ("""portfolio\S*"""r) ) ^^ {
+        case _ ~ _ => Portfolio
+    }
+
+    lazy val amount = decimalNumber ^^ {
+        case numText => BigDecimal(numText)
+    }
+
+    lazy val dollarAmount = (
+        amount
+        | "$" ~ amount ^^ {
+            case _ ~ amount => amount
+        }
+    )
+
+    lazy val ticker = ident
+
+    implicit def toCSR(s: String): CharSequenceReader = new CharSequenceReader(s)
 }
 
