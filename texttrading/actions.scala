439ea2beb35ba6b62c37e4ce93cbe5b81a808b0e
Cody Schafer <cpschafer@gmail.com>
2011-10-26 20:26:33 -0500

    Add structure for commands that return string data.

--- texttrading/actions.scala
+++ texttrading/actions.scala
@@ -1,7 +1,7 @@
 
 package texttrading
 
-import model.{Dollars, Shares}
+import model.{Dollars, Shares, Schema}
 
 // Message in from the frontend:
 
@@ -13,7 +13,6 @@ case class Message(
 
 trait Reply {
     def reply(text: String): Unit
-    
     def apply(text: String) = reply(text)
 }
 
@@ -24,19 +23,21 @@ case class Request(username: String, action: Action)
 // Actions that can be performed by the user:
 
 sealed abstract class Action
+case object Portfolio extends Action
 case class Buy(asset: StockAsset) extends Action
 case class Sell(asset: StockAsset) extends Action
 case class SellAll(ticker: String) extends Action
 
 sealed abstract class StockAsset
 case class StockDollars(ticker: String, dollars: Dollars) extends StockAsset
-case class StockShares (ticker: String, shares:  Shares) extends StockAsset
+case class StockShares(ticker: String, shares: Shares) extends StockAsset
 
 // Responses that can be sent back:
-
 case class Response(status: Status, extraMsgs: Seq[String])
 
 sealed abstract class Status
 case object OK extends Status
+case class TransactionResponse(sa :Schema.StockAsset) extends Status
+case class StringResponse(msg: String) extends Status
 case class Failed(msg: String) extends Status
 
--- texttrading/backend.scala
+++ texttrading/backend.scala
@@ -7,23 +7,24 @@ import model.Schema._
 import scalaz.Scalaz._
 
 class PitFailBackend extends Backend {
-    
+
     def perform(request: Request) = {
         val Request(username, action) = request
         val (user, greeting) = byUsername(username) match {
             case Some(user) => (user, Nil)
             case None       => (ensureUser(username), welcomeGreeting(username))
         }
-        
-        val status = action match {
+
+        val status : Status = action match {
             case Buy(asset)      => WithUser(user).buy(asset)
             case Sell(asset)     => WithUser(user).sell(asset)
             case SellAll(ticker) => WithUser(user).sellAll(ticker)
+            case Portfolio       => WithUser(user).portfolio
         }
-        
+
         Response(status, greeting)
     }
-    
+
     def welcomeGreeting(name: String) = Seq(
         """|Welcome to PitFail, a made-up money trader.
            |Try it out:
@@ -35,7 +36,7 @@ class PitFailBackend extends Backend {
 }
 
 case class WithUser(user: User) {
-    
+
     def failed: PartialFunction[Any,Failed] = {
         case e => Failed(standardMessage(e))
     }
@@ -48,23 +49,26 @@ case class WithUser(user: User) {
                     user.mainPortfolio.buyStock(ticker, shares)
                  case StockDollars(ticker, dollars) =>
                     user.mainPortfolio.buyStock(ticker, dollars)
-               }
-            |> { _ => OK }
+            }
+            |> TransactionResponse
         )
         catch failed
 
     def sell(asset: StockAsset) =
         try (
                asset
-            |> { case StockShares(ticker, shares) =>
+            |> {
+               case StockShares(ticker, shares) =>
                     user.mainPortfolio.sellStock(ticker, shares)
-                 case StockDollars(ticker, dollars) =>
+               case StockDollars(ticker, dollars) =>
                     user.mainPortfolio.sellStock(ticker, dollars)
                }
             |> { _ => OK }
         )
         catch failed
-        
+
+    def portfolio = StringResponse("This should be your portfolio, sorry.")
+
     def sellAll(ticker: String) =
         try {
             user.mainPortfolio.sellAll(ticker)
