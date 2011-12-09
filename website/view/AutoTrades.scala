f3bd2c76e49b76e9e251eb921673230137d03dd0
Owen Healy <ellbur@gmail.com>
2011-12-09 04:37:26 -0500

    Auto trades look much better

--- website/view/AutoTrades.scala
+++ website/view/AutoTrades.scala
@@ -21,6 +21,8 @@ import model.schema._
 
 class AutoTrades extends Page with Loggable {
     
+    val currentParam = S param "trade"
+    
     def render = jsapi.jsFuncDefs ++ refreshable.render
     val refreshable = Refreshable(doRender)
     
@@ -31,83 +33,102 @@ class AutoTrades extends Page with Loggable {
         try main(currentPortfolio)
         catch { 
             case NotLoggedIn =>
-                <p>Login to use auto trades</p>
+                <div class="block">
+                    <p>Login to use auto trades</p>
+                </div>
         }
     }
     
     def main(port: Portfolio) = {
-        lazy val content =
-            <div class="block">
-                <p>{newAutoTrade}</p>
-                {autoTradeList}
-            </div>
+        val autoTrades = port.myAutoTrades sortBy (_.title)
         
-        lazy val newAutoTrade = FormSubmit.rendered("Make new AutoTrade") {
-            port.userMakeNewAutoTrade()
-            refreshable.refresh
-        }
-        
-        lazy val autoTradeList = port.myAutoTrades map renderAutoTrade _
-        
-        content
+        if (autoTrades.isEmpty) createButton(port)
+        else tradeEditor(port, autoTrades)
     }
     
-    def renderAutoTrade(trade: AutoTrade) = {
+    def tradeEditor(port: Portfolio, autoTrades: List[AutoTrade]) = {
         val key = java.util.UUID.randomUUID.toString
         
-        val title = trade.title
-        val code  = trade.code
+        val current = currentParam flatMap { c =>
+            autoTrades filter (_.id == c) headOption
+        } openOr autoTrades.head
+    
+        lazy val tabs = autoTrades map { autoTrade =>
+            if (autoTrade == current)
+                <div class="tab active">
+                    <p>{titleField.main & <input class="blank"/>}</p>
+                </div>
+            else
+                <div class="tab inactive">
+                    <p><a href={"?trade="+autoTrade.id}>{autoTrade.title}</a></p>
+                </div>
+        }
         
+        lazy val titleField = StringField(current.title)
+        lazy val bodyField = TextAreaField(current.code)
+    
         lazy val form: Form[AutoTradeSubmit] = Form(AutoTradeSubmit,
             (
                 titleField,
                 bodyField
             ),
-            <div class="auto-trade-form">
-                <div class="auto-trade-title">{titleField.main} {titleField.errors}</div>
-                <div class="auto-trade-body">{bodyField.main & <textarea id={"code-"+key}/>}</div>
-                <div class="auto-trade-controls">
-                    {saveButton.main} {saveButton.errors}
-                    {executeButton.main} {executeButton.errors}
+            (
+                <div class="tab-bar">
+                    {tabs}
                 </div>
-            </div>
+                <div class="tab-pane">
+                    {bodyField.main &
+                        <textarea id={"code-"+key} class="auto-trade-code"/>
+                    }
+                </div>
+                <div class="auto-trade-controls">
+                    {executeButton.main}
+                    {saveButton.main}
+                    {deleteButton.main}
+                </div>
+            )
         )
         
-        lazy val titleField = StringField(title)
-        lazy val bodyField = TextAreaField(code)
-        
-        lazy val saveButton = Submit(form, "Save", refresh=false) { result =>
-            trade.userModify(result.title, result.code)
+        lazy val executeButton = Submit(form, "Run", refresh=false) {
+            case AutoTradeSubmit(title, code) =>
+                current.userModify(title, code)
+                logger.info("Executing " + code)
+                Call("runAutoTrade", Str(key))
         }
         
-        lazy val executeButton = Submit(form, "Run", refresh=false) { result =>
-            trade.userModify(result.title, result.code)
-            logger.info("Executing " + result.code)
-            Call("runAutoTrade", Str(key))
+        lazy val saveButton = Submit(form, "Save", refresh=false) {
+            case AutoTradeSubmit(title, code) =>
+                current.userModify(title, code)
         }
         
-        lazy val outputPane = {
+        lazy val deleteButton = Submit(form, "Delete", refresh=false) { _ =>
+            current.userDelete()
+            refreshable.refresh
+        }
+        
+        val output =
             <div id={"pane-"+key} class="auto-trade-output">
-                <pre id={"output-"+key} class="auto-trade-output">Stuff goes here</pre>
+                <pre id={"output-"+key} class="auto-trade-output">
+                
+                </pre>
             </div>
-        }
         
-        <div class="auto-trade">
-            <table class="auto-trade-table">
-                <col class="auto-trade-left-col"/>
-                <col class="auto-trade-right-col"/>
-                <tr>
-                    <td>Code:</td>
-                    <td>Output:</td>
-                </tr>
-                <tr>
-                    <td class="auto-trade-left">{form.render}</td>
-                    <td class="auto-trade-right">{outputPane}</td>
-                </tr>
-            </table>
-        </div>
+        val all =
+            <div class="block">
+                {createButton(port)}
+                {form.render}
+                {output}
+            </div>
+            
+        all
     }
     
+    def createButton(port: Portfolio) =
+        FormSubmit.rendered("Create AutoTrade") {
+            port.userMakeNewAutoTrade()
+            refreshable.refresh
+        }
+
     case class AutoTradeSubmit(
         title: String,
         code: String
