cd78a38b2346682339be53b3f178bfad4e84642b
Owen Healy <ellbur@gmail.com>
2011-12-14 16:38:22 -0500

    sorry... so sorry

---
+++ server/keys/Twitter.scala
@@ -0,0 +1,31 @@
+
+package code
+package keys
+
+/* API keys for pitfail_app1
+ *
+ * This file does not belong in source control. It is .gitignore'd.
+ *
+ */
+object Twitter {
+    val consumerKey     = "JZeuZEErkTC1IknbWAfOYw"
+    val consumerSecret  = "QRXHtGTLRrBcKu9q9Vi5vYiJVd92NZLFmh7nUevnQyg"
+
+    val accessToken = "371457808-uWoKV07rIcptEmHie4n4hCQQ7sGVS2Abtl5djb0V"
+    val accessTokenSecret = "9Fa4PqfzDkUXp4xG5xiSMz4yKjDlSv3FmpedXh3nNQ4"
+    val accessUser = "pitfail"
+}
+
+/* API keys for pf_frontend
+ *
+ * This file does not belong in source control. It is .gitignore'd.
+ *
+ */
+object RWTwitter {
+    val consumerKey     = "JNLv3DtNcVMHekOdv0rBDg"
+    val consumerSecret  = "U1gdVqWhVGqne1w6am0LKjv2E9aaupV6bLLaLsPgU"
+
+    val accessToken = "371457808-CYnhk5C1Ownzx054VRSuq0NXwYk2XDYSydMejfjI"
+    val accessTokenSecret = "U622G3rXHjmhyYxoa23XIprfeFcc1rIilmY4DSlvd4"
+    val accessUser = "pitfail"
+}
